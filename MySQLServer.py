4bfce751b05200f9c1dd90990c92b1c31579a26b
hidaya <hidaya.regragui@gmail.com>
2025-06-29 11:15:31 +0100

    task1

---
+++ MySQLServer.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(
+        host="172.25.96.1",
+        user="root",
+        password=""
+    )
+
+    if connection.is_connected():
+        cursor = connection.cursor()
+
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+    print(f"Error while connecting to MySQL: {e}")
+
+finally:
+    if 'cursor' in locals() and cursor:
+        cursor.close()
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
+        print("MySQL connection is closed.")
